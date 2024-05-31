3c6e873961e8a7eb22d902a6a0cfc869f5a19f27
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-05-31 14:33:41 +0800

    Setup Main Service for Eureka

---
+++ gaminatorium-main/src/test/java/eu/gaminatorium/base/GaminatoriumMainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.gaminatorium.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaminatoriumMainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
