6290e0bd9259c801edf1986163d0dfd443d15aeb
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-05-31 14:47:20 +0800

    Setup Zuul Gateway

---
+++ zuul-gateway/src/main/groovy/eu/gaminatorium/gateway/GatewayApplication.groovy
@@ -0,0 +1,13 @@
+package eu.gaminatorium.gateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GatewayApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GatewayApplication, args)
+	}
+
+}
