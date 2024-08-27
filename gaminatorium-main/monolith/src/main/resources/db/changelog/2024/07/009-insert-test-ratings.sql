234186267c46c50af7010964931f3f15370b4e86
MichalG315 <grot.michal@wp.pl>
2024-08-27 18:21:25 +0200

    Final of liquibase migrations.

--- gaminatorium-main/monolith/src/main/resources/db/changelog/2024/07/009-insert-test-ratings.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeSet Slawek84PL:009
-
-INSERT INTO game_rating (score, comment, posting_date, game_id, user_id)
-VALUES (4, 'spoko', '2024-06-01', 1, 1),
-       (1, 'shitty', '2024-06-05', 2, 2),
-       (2, 'boring', '2024-06-06', 3, 3)
-       ;
